17ef17bb95e3acb24361c1b0e919b73400d3c5a2
Rooseveltson cebeat <rooseveltsoncebeat@yahoo.fr>
2024-06-23 08:07:02 +0200

    feat: b2 config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Imports
 var express = require('express');
 var bodyParser = require('body-parser');
-var apiRouter = require('../apiRouter').router;
+var apiRouter = require('./apiRouter').router;
 
 // Instanciate server
 var server = express();
